7f30b52e3b5dc930027f6fcc7801d7df8f2a027d
anseki <AnSeki.aff@gmail.com>
2018-07-27 17:27:51 +0900

    Add test for `this` in handlers

--- test/spec/stopPropagation.js
+++ test/spec/stopPropagation.js
@@ -3,6 +3,7 @@ describe('stopPropagation', function() {
 
   var pageDone, PointerEvent, traceLog, utils,
     elmChild, elmParent, pointerEvent,
+    nestStartHandler, nestMoveHandler, nestEndHandler, nestCancelHandler,
 
     X1 = 1,
     Y1 = 2,
@@ -28,10 +29,19 @@ describe('stopPropagation', function() {
 
       pointerEvent = new PointerEvent();
       pointerEvent.addStartHandler(elmChild,
-        pointerEvent.regStartHandler(function() { return true; }));
-      pointerEvent.addMoveHandler(elmChild, function() {});
-      pointerEvent.addEndHandler(elmChild, function() {});
-      pointerEvent.addCancelHandler(elmChild, function() {});
+        pointerEvent.regStartHandler(function() {
+          if (nestStartHandler) { nestStartHandler(this); } // eslint-disable-line no-invalid-this
+          return true;
+        }));
+      pointerEvent.addMoveHandler(elmChild, function() {
+        if (nestMoveHandler) { nestMoveHandler(this); } // eslint-disable-line no-invalid-this
+      });
+      pointerEvent.addEndHandler(elmChild, function() {
+        if (nestEndHandler) { nestEndHandler(this); } // eslint-disable-line no-invalid-this
+      });
+      pointerEvent.addCancelHandler(elmChild, function() {
+        if (nestCancelHandler) { nestCancelHandler(this); } // eslint-disable-line no-invalid-this
+      });
 
       elmParent = pageDocument.getElementById('parent');
       ['mousedown', 'mousemove', 'mouseup'].forEach(function(type) {
@@ -180,6 +190,237 @@ describe('stopPropagation', function() {
           '</endListener>'
         ]);
 
+        done();
+      }, TIME_SPAN);
+    }, TIME_SPAN);
+  });
+
+  it('start(stopPropagation:OFF -> ON in handler)', function(done) {
+    nestStartHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = true;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = false;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      expect(traceLog).toEqual([
+        '<startListener>', 'type:mousedown', 'curPointerClass:null',
+
+        'CHANGE:options.stopPropagation',
+
+        'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+        '</startListener>'
+      ]);
+
+      nestStartHandler = null;
+      done();
+    }, TIME_SPAN);
+  });
+
+  it('move(stopPropagation:OFF -> ON in handler)', function(done) {
+    nestMoveHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = true;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = false;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      utils.fireMouseEvent(elmChild, 'mousemove', {clientX: X2, clientY: Y2, bubbles: true});
+      setTimeout(function() {
+        expect(traceLog).toEqual([
+          '<startListener>', 'type:mousedown', 'curPointerClass:null',
+          'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+          '</startListener>',
+
+          '<parent:mousedown/>',
+
+          '<moveListener>', 'type:mousemove', 'curPointerClass:mouse',
+          '<move>',
+          'lastPointerXY:(' + X2 + ',' + Y2 + ')',
+
+          'CHANGE:options.stopPropagation',
+
+          '</move>',
+          '</moveListener>'
+        ]);
+
+        nestMoveHandler = null;
+        done();
+      }, TIME_SPAN);
+    }, TIME_SPAN);
+  });
+
+  it('end(stopPropagation:OFF -> ON in handler)', function(done) {
+    nestEndHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = true;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = false;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      utils.fireMouseEvent(elmChild, 'mouseup', {clientX: X2, clientY: Y2, bubbles: true});
+      setTimeout(function() {
+        expect(traceLog).toEqual([
+          '<startListener>', 'type:mousedown', 'curPointerClass:null',
+          'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+          '</startListener>',
+
+          '<parent:mousedown/>',
+
+          '<endListener>', 'type:mouseup', 'curPointerClass:mouse',
+          '<end>',
+          'lastPointerXY:(' + X2 + ',' + Y2 + ')',
+
+          'CHANGE:options.stopPropagation',
+
+          'curPointerClass:null',
+          '</end>',
+          '</endListener>'
+        ]);
+
+        nestEndHandler = null;
+        done();
+      }, TIME_SPAN);
+    }, TIME_SPAN);
+  });
+
+  it('start(stopPropagation:ON -> OFF in handler)', function(done) {
+    nestStartHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = false;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = true;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      expect(traceLog).toEqual([
+        '<startListener>', 'type:mousedown', 'curPointerClass:null',
+
+        'CHANGE:options.stopPropagation',
+
+        'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+        '</startListener>',
+
+        '<parent:mousedown/>'
+      ]);
+
+      nestStartHandler = null;
+      done();
+    }, TIME_SPAN);
+  });
+
+  it('move(stopPropagation:ON -> OFF in handler)', function(done) {
+    nestMoveHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = false;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = true;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      utils.fireMouseEvent(elmChild, 'mousemove', {clientX: X2, clientY: Y2, bubbles: true});
+      setTimeout(function() {
+        expect(traceLog).toEqual([
+          '<startListener>', 'type:mousedown', 'curPointerClass:null',
+          'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+          '</startListener>',
+
+          '<moveListener>', 'type:mousemove', 'curPointerClass:mouse',
+          '<move>',
+          'lastPointerXY:(' + X2 + ',' + Y2 + ')',
+
+          'CHANGE:options.stopPropagation',
+
+          '</move>',
+          '</moveListener>',
+
+          '<parent:mousemove/>'
+        ]);
+
+        nestMoveHandler = null;
+        done();
+      }, TIME_SPAN);
+    }, TIME_SPAN);
+  });
+
+  it('end(stopPropagation:ON -> OFF in handler)', function(done) {
+    nestEndHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('CHANGE:options.stopPropagation');
+      that.options.stopPropagation = false;
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = true;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      utils.fireMouseEvent(elmChild, 'mouseup', {clientX: X2, clientY: Y2, bubbles: true});
+      setTimeout(function() {
+        expect(traceLog).toEqual([
+          '<startListener>', 'type:mousedown', 'curPointerClass:null',
+          'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+          '</startListener>',
+
+          '<endListener>', 'type:mouseup', 'curPointerClass:mouse',
+          '<end>',
+          'lastPointerXY:(' + X2 + ',' + Y2 + ')',
+
+          'CHANGE:options.stopPropagation',
+
+          'curPointerClass:null',
+          '</end>',
+          '</endListener>',
+
+          '<parent:mouseup/>'
+        ]);
+
+        nestEndHandler = null;
+        done();
+      }, TIME_SPAN);
+    }, TIME_SPAN);
+  });
+
+  it('`this` in cancel()', function(done) {
+    nestCancelHandler = function(that) {
+      expect(that).toBe(pointerEvent);
+      traceLog.push('nestCancelHandler');
+    };
+
+    resetData();
+    pointerEvent.options.stopPropagation = false;
+    utils.fireMouseEvent(elmChild, 'mousedown', {clientX: X1, clientY: Y1, bubbles: true});
+    setTimeout(function() {
+      pointerEvent.cancel();
+      setTimeout(function() {
+        expect(traceLog).toEqual([
+          '<startListener>', 'type:mousedown', 'curPointerClass:null',
+          'curPointerClass:mouse', 'lastPointerXY:(' + X1 + ',' + Y1 + ')',
+          '</startListener>',
+
+          '<parent:mousedown/>',
+
+          '<cancel>',
+
+          'nestCancelHandler',
+
+          'curPointerClass:null',
+          '</cancel>'
+        ]);
+
+        nestCancelHandler = null;
         done();
       }, TIME_SPAN);
     }, TIME_SPAN);
